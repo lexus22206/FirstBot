5c2a6eb38efa4693fe7ff72c6386bdad9bbd03ee
lexus22206 <lexusp715@gmail.com>
2025-09-10 17:29:19 +0300

    webhook

--- bot.js
+++ bot.js
@@ -4,12 +4,13 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '8287055755:AAFDyxXqAEr6iw0Jc3IZYxJHwIL_4hnbqGM';
 const bot = new TelegramBot(token);
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 //URL з ngrok
 const url = 'https://9a5d27ff2e4c.ngrok-free.app';
-const port = 3000;
+
 
 //встановлюємо webhook
 bot.setWebHook("https://firstbot-san3.onrender.com" + token);
@@ -34,5 +35,5 @@ bot.on("message", (msg) => {
 
 //запускаємо сервер
 app.listen(port, () => {
-    console.log(`Express server running on port ${port}`);
+    console.log("Bot ranning on port", port);
 });
